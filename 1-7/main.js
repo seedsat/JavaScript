717aecffa1b9941b3254c785ceddae704b74a66d
seedsat <e.satoshi.bi@gmail.com>
2021-09-20 13:20:11 +0900

    課題提出テスト
    クラスとインスタンス

---
+++ 1-7/main.js
@@ -0,0 +1,12 @@
+class Taiyaki {
+    constructor(nakami) {
+        this.nakami = nakami;
+    }
+}
+
+let anko = new Taiyaki('あんこ');
+console.log('中身は' + anko.nakami + 'です');
+let creem = new Taiyaki('クリーム');
+console.log('中身は' + creem.nakami + 'です');
+let cheez = new Taiyaki('チーズ');
+console.log('中身は' + cheez.nakami + 'です');
